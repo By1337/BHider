9bf9ffb9c38ef5b7eba0478f1a7e440ba6a4d24b
By1337 <by133700@gmail.com>
2025-01-19 06:19:38 +0300

    init

---
+++ src/main/java/dev/by1337/hider/BHider.java
@@ -0,0 +1,20 @@
+package dev.by1337.hider;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BHider extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
